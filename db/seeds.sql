b6c46383dedd15cf5bd65173c537906b26baa4d0
daevidvo <itsdavidvo@gmail.com>
2023-04-28 14:32:40 -0700

    finished building out functionality

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,5 @@
 INSERT INTO movie_info (movie_name)
 VALUES ("Citizen Kane"), ("Casablanca"), ("The Godfather");
+
+INSERT INTO review_info (review, movie_id)
+VALUES ("this was great!", 1), ("I haven't seen this movie yet but 11/10", 2), ("I love garlic", 3);
--- server.js
+++ server.js
@@ -17,24 +17,88 @@ const db = mysql.createConnection(
     console.log('Connected to movie_db')
 );
 
-db.query('\. schema.sql', (err, results)=>{
-    if(err){
-      console.log(err)
-    }
-    console.log(results)
-})
-
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //routes
 
-app.post('/api/add-movie', (req,res)=>{
-    console.log(req.body)
+app.get('/api/movies', (req,res)=>{
+    db.query('SELECT movie_name FROM movie_info', (err,results)=>{
+        if(err){
+            console.log(err);
+        }
+        res.send(results)
+    })
 })
 
+app.post('/api/add-movie', (req,res)=>{
+    if(req.body){
+        console.log(req.body.movie_name)
+        let newMovie = req.body.movie_name
 
+        db.query(`INSERT INTO movie_info (movie_name) VALUE (?)`, newMovie, (err,results)=>{
+            if(err){
+                console.log(err)
+            } else{
+                console.log('movie successfully added into db')
+            }
+        })
+        db.query('SELECT movie_name FROM movie_info', (err,results)=>{
+            if(err){
+                console.log(err);
+            }
+            res.send(results)
+        })
+    }
+})
+
+app.delete('/api/movie/:id',(req,res)=>{
+    let movieId = req.params.id
+
+    db.query(`DELETE FROM movie_info WHERE id = ?`, movieId, (err,results)=>{
+        if(err){
+            console.log(err)
+        }else{
+            console.log(`movie with the id of ${movieId} has been deleted`)
+        }
+        // console.log(results)
+    })
+    db.query('SELECT * FROM movie_info', (err,results)=>{
+        if(err){
+            console.log(err);
+        }
+        res.send(results)
+    })
+})
+
+app.get('/api/movie-reviews',(req,res)=>{
+    db.query('SELECT movie_name, review_info.review AS review FROM movie_info INNER JOIN review_info ON movie_info.id = review_info.movie_id', (err,results)=>{
+        if(err){
+            console.log(err);
+        }
+        res.send(results)
+    })
+})
+
+app.put('/api/review/:id',(req,res)=>{
+    let movieId = req.params.id
+    let reviewChange = req.body.updatedReview
+
+    db.query(`UPDATE review_info SET review = ? WHERE id = ?`, [reviewChange, movieId],(err,results)=>{
+        if(err){
+            console.log(err)
+        }else{
+            console.log(`movie with the id of ${movieId}'s review has been updated`)
+        }
+    })
+    db.query('SELECT movie_name, review_info.review AS review FROM movie_info INNER JOIN review_info ON movie_info.id = review_info.movie_id', (err,results)=>{
+        if(err){
+            console.log(err);
+        }
+        res.send(results)
+    })
+})
 
 
 
